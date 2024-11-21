5b07b9100a1d970ad9572cef7431e8f447ca6b45
wolfxlover_2 <150090168+wolfywolfywolfy@users.noreply.github.com>
2024-11-20 17:14:36 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,88 @@
-let position = 0; 
-
-//display first item in list (first equation)
-//window.onload = function() {
-    //displayEquation(0);
-    //document.getElementById("clickPrev").disabled = true;
-//}
 
 //my entries INITIALIZED or declared..not sure.
 let myEntries = [];
 
-const myEquation = document.getElementById("equationName").value; 
-const subject = document.getElementById("subjectName").value;
-const studied = document.getElementById("studied").checked;
-
+//I don't know what this is for.
 const myEntry = {
-    myEquation: equation,
-    subject: subject,
-    studied: studied
+    myCharacter: characterName,
+    mySong: songName,
+    listened: listened
 };
 
-//display table
-function displayTable() {
-    //make new row after each button click
-    
-}
-
-//make a table
+//make a table upon loading. 
 function makeTable() {
-    //make table element based off id in html
+    //associate entries with variable.
+    myEntry.myCharacter = document.getElementById("characterName").value; 
+    myEntry.mySong = document.getElementById("songName").value;
+    myEntry.listened = document.getElementById("listened").checked ? "Yes" : "No";
+
+    //make table element based off div id location in html
     const theTable = document.getElementById("myTable");
+
+    //assign variable to ID touhou table (then test if it exists)
+    let aTable = document.getElementById("touhouTable");
+
+    if(!aTable) {
+        //create a table to be used
+        aTable = document.createElement("table")
+        //assign an id to created table
+        aTable.id = "touhouTable";
+
+        //create header row
+        const headerRow = document.createElement("tr");
+    
+        //add titles to the header cells
+        let header1 = document.createElement("th");
+        header1.textContent = "song";
+
+        let header2 = document.createElement("th");
+        header2.textContent = "character";
+
+        let header3 = document.createElement("th");
+        header3.textContent = "listened?";
+
+        //add the table data to each header
+        headerRow.appendChild(header1);
+        headerRow.appendChild(header2);
+        headerRow.appendChild(header3);
+
+        //add the row to the table
+        aTable.appendChild(headerRow);
+        theTable.appendChild(aTable);
+    }
+    //assign entry to row and add row to table. (reference function below)
+    let row = addTableRow(myEntry);
+    aTable.appendChild(row);
+
+
+    //theTable.appendChild(aTable);
 }
 
 //add a row to table
-function addTableRow() {
+function addTableRow(myEntry) {
     //when button is clicked, make a row
     const row = document.createElement("tr");
+
+    //add data to row
+    let tableData1 = document.createElement("td");
+    tableData1.textContent = myEntry.mySong;
+
+    let tableData2 = document.createElement("td");
+    tableData2.textContent = myEntry.myCharacter;
+
+    let tableData3 = document.createElement("td");
+    tableData3.textContent = myEntry.listened;
+
+    //add the data to each row
+    row.appendChild(tableData1);
+    row.appendChild(tableData2);
+    row.appendChild(tableData3);
+
+    //send this row to makeTable function
+    return row;
 }
 
-//display the table
-
-
 //handle the submit button
+document.getElementById("addToTable").onclick = function(){
+    makeTable();
+};
